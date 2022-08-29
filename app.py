d7f9c0a2f25d59876936c4074d459e70a1569a3f
Daniel Sosa <danielsosa521@utexas.edu>
2022-08-29 15:50:07 -0500

    Astronaut: Added API imports

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask
+__author__ = "Daniel Sosa"
+__date__ = "April 14, 2022"
+
+from datetime import datetime
+import calendar
+import random
+from pymongo import MongoClient
+
+#NOTE : Need this package for MongoClient init
+#Without it, SSL CERTIFICATE VERIFY FAILED EXCEPTION
+#Should find a better solution but i got fed up lol
+# import certifi
+import database
+
+from flask import Flask, jsonify
+from flask_restful import Api, Resource
 app = Flask(__name__)
+api = Api(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World! API on Render now. API build code : Astronaut'
